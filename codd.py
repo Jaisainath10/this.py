55555e2cf30310553641f9519b4d7cbbd3779968
Jaisainath10 <83420534+Jaisainath10@users.noreply.github.com>
2023-04-19 23:10:29 +0530

    Update codd.py

--- codd.py
+++ codd.py
@@ -5,11 +5,11 @@ def largest_number(a, b, c):
     return max(a, b, c)
 
 # Streamlit interface
-st.title("Find the largest number")
-a = st.number_input("Enter the first number:", value=0, step=1)
-b = st.number_input("Enter the second number:", value=0, step=1)
-c = st.number_input("Enter the third number:", value=0, step=1)
+st.title("Find the largest number among three")
+a = st.number_input("Enter the first number:")
+b = st.number_input("Enter the second number:")
+c = st.number_input("Enter the third number:")
 
-if st.button("Find largest number"):
+if st.button("largest number"):
     result = largest_number(a, b, c)
     st.write(f"The largest number is {result}")
